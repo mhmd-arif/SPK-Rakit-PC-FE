934ef10e2328a0a7e8573ca9784bc5d913bdfb59
mhmd-arif <mhmdarif.developer@gmail.com>
2023-06-10 22:41:31 +0700

    add table

--- spk-rakit-pc-fe/src/components/Build.jsx
+++ spk-rakit-pc-fe/src/components/Build.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { bgBuild } from "../assets";
 import { Select, Checkbox, Input, Button } from '@chakra-ui/react'
+import TableResult from "./TableResult";
+import { useFetch } from "../helpers/useFetch";
 
 const Build = () => {
   const [showDiv, setShowDiv] = useState(false);
@@ -8,6 +10,55 @@ const Build = () => {
   const handleCheckboxChange = () => {
     setShowDiv(!showDiv);
   };
+
+  const [refreshSignal, setRefreshSignal] = useState(false);
+  const { data: vouchersData } = useFetch("/vouchers", refreshSignal);
+  const [id, setId] = useState();
+
+  const [vouchers, setVouchers] = useState([]);
+  useMemo(() => {
+    if (!vouchersData?.data?.vouchers) return;
+    setVouchers(vouchersData.data.vouchers);
+  }, [vouchersData]);
+
+  function rpFormatter(num) {
+    return 'Rp' + Intl.NumberFormat('en-DE').format(num)
+  }
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'No',
+        Cell: ({ row, flatRows }) => {
+          return flatRows.indexOf(row) + 1;
+        },
+        width: 30,
+      },
+      {
+        Header: "Komponen",
+        accessor: "Komponen",
+        width: 80,
+      },
+      {
+        Header: "Nama komponen",
+        accessor: "namakomponen",
+        width: 100,
+      },
+      {
+        Header: "Spesifikasi",
+        accessor: "spesifikasi",
+        width: 200,
+      },
+      {
+        Header: "Harga",
+        accessor: "harga",
+        Cell: ({ value }) => {
+          return rpFormatter(value);
+        },
+        width: 100,
+      },
+    ], []
+  );
 
   return (
     <div className="Build w-full min-h-screen font-poppins text-black bg-[#F4F4F9] bg-no-repeat bg-contain bg-fixed" style={{ backgroundImage: `url(${bgBuild})` }} >
@@ -71,10 +122,12 @@ const Build = () => {
 
         <Button bg='#B8D0DB'>Rakit</Button>
 
+
       </div>
+      <div className="">
+        <TableResult columns={columns} data={vouchers} setRefreshSignal={setRefreshSignal} />
 
-
-
+      </div>
 
     </div>
   );
