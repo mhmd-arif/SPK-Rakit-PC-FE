9e2bbad15559128d1581d54f51fa01318344a32e
mhmd-arif <mhmdarif.developer@gmail.com>
2023-06-11 14:23:09 +0700

    cek

--- spk-rakit-pc-fe/src/assets/index.js
+++ spk-rakit-pc-fe/src/assets/index.js
@@ -1,6 +1,7 @@
 import ReactSvg from "./react.svg"
+import bgBuild from "./bgBuild.svg"
 import logoRakitPc from "./logoRakitPc.svg"
 import HeroImg from "./HeroImg.svg"
-import bgBuild from "./bgBuild.svg"
+
 
 export { ReactSvg, HeroImg, logoRakitPc, bgBuild }
